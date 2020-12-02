3a28ae6fe5e19fee11fb67c30ce66caba1589584
Krzysztof Cel <krzycel789@student.polsl.pl>
2020-12-02 08:35:07 +0100

    bez wsk

--- L06.cpp
+++ L06.cpp
@@ -1,10 +1,25 @@
-﻿
-
-#include <iostream>
+﻿#include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    char tekst[100],t2[100];
+    cout << "wiadomosc: ";
+    fgets(tekst, 100, stdin);
+    int i = 0;
+    while (i < strlen(tekst) - 1)
+    {
+        if (tekst[i] >= 'a' && tekst[i] <= 'z' || tekst[i] >= 'A' && tekst[i] <= 'Z')
+            t2[i] = tekst[i];
+        else
+        {
+            t2[i] = ' ';
+        }
+        i++;
+    }
+    t2[i] = '\0';
+    cout << t2;
+
 }
 
 
