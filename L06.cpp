bc541d55370dc49ac91c30a425c24f2cfa19f3d4
Krzysztof Cel <krzycel789@student.polsl.pl>
2020-12-02 09:33:22 +0100

    wsk

--- L06.cpp
+++ L06.cpp
@@ -4,36 +4,31 @@ using namespace std;
 int main()
 {
     char tekst[100];
-    char *t1, *t2;
+    char *t1;
+    char *t2;
     cout << "wiadomosc: ";
     fgets(tekst, 100, stdin);
 
     int i = 0;
     t1 = &tekst[0];
-    
-    for (i = 0; i < strlen(tekst) - 1; i++)
-    {
+    t2 = &tekst[0];
+    for (i = 0; i < strlen(tekst); i++)
         *t1 = tekst[i];
-        t1++;
-    }
     t1 = &tekst[0];
-    
-    while (i < strlen(t1) - 1)
+    for (t1=tekst;t1<tekst+strlen(tekst);t1++)
     {
         if (*t1 >= 'a' && *t1 <= 'z' || *t1 >= 'A' && *t1 <= 'Z')
         {
-            
             *t2 = *t1;
         }
         else
         {
             *t2 = ' ';
         }
-        i++;
     }
-
     *t2 = '\0';
-    cout << t2;
+    
+    cout << "Tekst: " << tekst << endl;
 
 }
 
